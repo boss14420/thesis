17bd532eb488b3ff6c81f16a8fe64096439081bc
boss14420 <firefoxlinux@gmail.com>
2015-03-18 23:59:51 +0700

    Initial commit

---
+++ CPU/common.hpp
@@ -0,0 +1,49 @@
+#ifndef __COMMON_HPP__
+#define __COMMON_HPP__
+
+#include <fstream>
+#include <cmath>
+#include <cstdint>
+
+template <typename T>
+void printColor(std::ostream &os, T value)
+{
+    std::uint8_t color = std::floor(255-255*value);
+//    std::uint8_t rgb[] = { color, color, color };
+    os.write(reinterpret_cast<char const*>(&color), 1);
+//    os << std::floor(255-255*value) << ' '
+//       << std::floor(255-255*value) << ' '
+//       << std::floor(255-255*value) << ' ';
+}
+
+template <typename T>
+void exportPixmap(T const *c, std::size_t width, std::size_t height, std::size_t stride, char const *filename)
+{
+    std::ofstream ofs(filename); //, std::ios::binary);
+    ofs << "P5\n";
+    ofs << width + 1 << ' ' << height + 1 << "\n255\n";
+
+    for (int j = height; j >= 0; --j) {
+        for (std::size_t i = 0; i <= width; ++i) {
+            printColor(ofs, c[j*stride + i]);
+        }
+    }
+
+//    T const *prow = c + width * (height - 1);
+//    for (; prow != c-width; prow -= width) {
+//        for (std::size_t x = 0; x != width; ++x)
+//            printColor(ofs, prow[x]);
+//    }
+    ofs.close();
+}
+
+template <typename T>
+T stddev(T const *c1, T const *c2, std::size_t width, std::size_t height) {
+    T sum = 0;
+    for (T const *c1tmp = c1; c1tmp != c1 + width*height; ++c1tmp, ++c2) {
+        sum += (*c1tmp - *c2) * (*c1tmp - *c2);
+    }
+    return std::sqrt(sum / (width * height));
+}
+
+#endif
---
+++ CPU/hydrodynamic-flow.cc
@@ -0,0 +1,240 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hydrodynamic-flow.cc
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  03/18/2015 11:23:52 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Boss14420 (), firefox at gmail dot com
+ *   Organization:
+ *
+ * =====================================================================================
+ */
+
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include "common.hpp"
+
+#define SWAP(x, y) (x ^= y ^= x ^= y);
+
+#define STRIDE 128
+#define WIDTH 100
+#define HEIGHT 100
+//#define dx .01f
+//#define dy .01f
+//#define dt .01f
+#define INDEXU(x, y) ((y) * (STRIDE) + (x))
+#define INDEXV(x, y) ((y) * (STRIDE) + (x))
+#define INDEXP(x, y) ((y)*STRIDE+ (x))
+#define SQR(x) ((x) * (x))
+#define nu 0.1f
+#define Dtolerance 0.05f
+
+
+/*
+   P[INDEXP(i, j)] <-- P[i, j]
+   uc[INDEXU(i, j)] <-- u[i-1/2, j]
+   vc[INDEXV(i, j)] <-- v[i, j-1/2]
+ */
+
+template <class T>
+class SimpleBoundary
+{
+private:
+public:
+    int x0, x1, y0, y1;
+    T InFlowU, InFlowV;
+
+public:
+    bool isInFlowBoundary(int i, int j) const { return i == 0; }
+    bool isOutFlowBoundary(int i, int j) const { return i == WIDTH; }
+    bool isFloorBoundary(int i, int j) const { return j == 0; }
+    bool isCeilingBoundary(int i, int j) const { return j == HEIGHT; }
+
+    bool isObstace(int i, int j) const {
+        return x0 <= i && i <= x1 && y0 <= j && j <= y1;
+    }
+
+    T inflowU() const { return InFlowU; }
+    T inflowV() const { return InFlowV; }
+};
+
+
+// finite differencing of u
+template <typename T>
+void DFu(T const *uc, T const *vc, T const *P, T dx, T dy, int i, int j, T &Du)
+{
+    Du = -1/dx*.25f*(SQR(uc[INDEXU(i+1,j)] + uc[INDEXU(i,j)]) - SQR(uc[INDEXU(i-1,j)] + uc[INDEXU(i,j)]));
+    Du += -1/dy*.25f*( (uc[INDEXU(i,j+1)]+uc[INDEXU(i,j)])*(vc[INDEXV(i+1,j)]+vc[INDEXV(i,j)])
+                      -(uc[INDEXU(i,j)]+uc[INDEXU(i,j-1)])*(vc[INDEXV(i+1,j-1)]+vc[INDEXV(i,j-1)]) );
+    Du += -1/dx*(P[INDEXP(i,j)] - P[INDEXP(i-1,j)]);
+    Du += nu*( (uc[INDEXU(i+1,j)] - 2*uc[INDEXU(i,j)] + uc[INDEXU(i-1,j)])/(dx*dx)
+                +(uc[INDEXU(i,j+1)] - 2*uc[INDEXU(i,j)] + uc[INDEXU(i,j-1)])/(dy*dy) );
+}
+
+// finite differencing of v (without dP/dy)
+template <typename T>
+void DFv(T const *uc, T const *vc, T const *P, T dx, T dy, int i, int j, T &Dv)
+{
+    Dv = -1/dy*.25f*(SQR(vc[INDEXV(i,j+1)] + vc[INDEXV(i,j)]) - SQR(vc[INDEXV(i,j-1)] + vc[INDEXV(i,j)]))
+          -1/dy*.25f*( (uc[INDEXU(i,j+1)]+uc[INDEXU(i,j)])*(vc[INDEXV(i+1,j)]+vc[INDEXV(i,j)])
+                      -(uc[INDEXU(i-1,j)]+uc[INDEXU(i-1,j+1)])*(vc[INDEXV(i,j)]+vc[INDEXV(i-1,j)]) )
+          -1/dy*(P[INDEXP(i,j)] - P[INDEXP(i,j-1)])
+          + nu*( (vc[INDEXV(i,j+1)] - 2*vc[INDEXV(i,j)] + vc[INDEXV(i,j-1)])/(dx*dx)
+                +(vc[INDEXV(i+1,j)] - 2*vc[INDEXV(i,j)] + vc[INDEXV(i-1,j)])/(dy*dy) );
+}
+
+template <typename T>
+T delta_P(T Dij, T beta)
+{
+    return -beta * Dij;
+}
+
+template <typename T, typename BoundaryCond>
+void update_uv(T const *uc, T const *vc, T const *P, T *un, T *vn, T dt, T dx, T dy, BoundaryCond bound)
+{
+    int i, j;
+    for (j = 0; j <= HEIGHT; ++j) {
+        for (i = 0; i <= WIDTH; ++i) {
+            if (bound.isInFlowBoundary(i, j)) {
+                // Velocity at inflow boundary is set to initial value
+                un[INDEXU(i, j)] = bound.inflowU();
+                vn[INDEXU(i, j)] = bound.inflowV();
+            } else if (bound.isOutFlowBoundary(i, j)) {
+                // Velocity at outflow boundary is set to velocity of the
+                // neighboring upstream cell
+                un[INDEXU(i, j)] = un[INDEXU(i-1, j)];
+                vn[INDEXU(i, j)] = vn[INDEXV(i-1, j)];
+            } else if (bound.isFloorBoundary(i, j)) {
+                // periodic boundary condition
+                // TODO: set to new velocity of ceiling cell
+                un[INDEXU(i, j)] = uc[INDEXU(i, HEIGHT-1)];
+                vn[INDEXU(i, j)] = vc[INDEXV(i, HEIGHT-1)];
+            } else if (bound.isCeilingBoundary(i, j)) {
+                // periodic boundary condition
+                un[INDEXU(i, j)] = un[INDEXU(i, 1)];
+                vn[INDEXU(i, j)] = vn[INDEXV(i, 1)];
+            } else if (bound.isObstace(i, j)) {
+                // zero velocity
+                un[INDEXU(i, j)] = 0;
+                vn[INDEXU(i, j)] = 0;
+            } else {
+                float Du, Dv;
+                DFu(uc, vc, P, dx, dy, i, j, Du);
+                un[INDEXU(i, j)] = uc[INDEXU(i, j)] + dt * Du;
+
+                DFv(uc, vc, P, dx, dy, i, j, Dv);
+                vn[INDEXV(i, j)] = vc[INDEXV(i, j)] + dt * Dv;
+            }
+        }
+    }
+}
+
+template <typename T, typename BoundaryCond>
+void time_step(T const *uc, T const *vc, T *P, T *un, T *vn, T dt, T dx, T dy, T beta, BoundaryCond bound)
+{
+    update_uv(uc, vc, P, un, vn, dt, dx, dy, bound);
+
+    T D, delta_P;
+    bool incompressible = false;
+    do {
+        incompressible = true;
+        for (int j = 0; j <= HEIGHT; ++j) {
+            for (int i = 0; i <= WIDTH; ++i) {
+                if (bound.isInFlowBoundary(i, j)) {
+                } else if (bound.isOutFlowBoundary(i, j)) {
+                    P[INDEXP(i, j)] = P[INDEXP(i-1, j)];
+                } else if (bound.isFloorBoundary(i, j)) {
+                } else if (bound.isCeilingBoundary(i, j)) {
+                    P[INDEXP(i, j)] = P[INDEXP(i, 1)];
+                } else if (bound.isObstace(i, j)) {
+                    P[INDEXP(i, j)] = 0;
+                } else {
+                    D = 1/dx * (uc[INDEXU(i+1,j)] - uc[INDEXU(i,j)])
+                        +1/dy * (vc[INDEXV(i,j+1)] - vc[INDEXV(i,j)]);
+                    if (D > Dtolerance) {
+//                        if (D > 1.5) {
+//                            std::cout << i << ", " << j << '\n';
+//                            std::exit(-1);
+//                        }
+                        delta_P = -beta * D;
+                        P[INDEXP(i,j)] += delta_P;
+                        un[INDEXU(i,j)] -= (dt/dx)*delta_P;
+                        un[INDEXU(i+1,j)] += (dt/dx)*delta_P;
+                        vn[INDEXV(i,j)] -= (dt/dy)*delta_P;
+                        vn[INDEXV(i,j+1)] += (dt/dy)*delta_P;
+                        incompressible = false;
+                    }
+                }
+            }
+            // TODO: Inflow, Floor boundaries
+        }
+    } while (!incompressible);
+}
+
+template <typename T, typename BoundaryCond>
+void initialize(T* &ucurrent, T* &vcurrent, T* &unew, T* &vnew, T* &P, BoundaryCond bound)
+{
+    ucurrent = (T*) std::calloc(STRIDE*STRIDE, sizeof(T));
+    vcurrent = (T*) std::calloc(STRIDE*STRIDE, sizeof(T));
+    unew = (T*) std::calloc(STRIDE*STRIDE, sizeof(T));
+    vnew = (T*) std::calloc(STRIDE*STRIDE, sizeof(T));
+    P = (T*) std::calloc(STRIDE*STRIDE, sizeof(T));
+
+    // inflow boundary
+    for (int j = 0; j <= HEIGHT; ++j) {
+        ucurrent[INDEXU(0, j)] = bound.inflowU();
+        vcurrent[INDEXU(0, j)] = bound.inflowV();
+    }
+}
+
+template <typename T>
+void print_velocity(T const *uc, T const *vc, int index)
+{
+    char name[20];
+    std::cout << "Step " << index << '\n';
+    std::snprintf(name, 19, "uc%04d.pgm", index);
+    exportPixmap(uc, WIDTH, HEIGHT, STRIDE, name);
+    std::snprintf(name, 19, "vc%04d.pgm", index);
+    exportPixmap(vc, WIDTH, HEIGHT, STRIDE, name);
+}
+
+template <typename T, typename BoundaryCond>
+void flow(T total_time, T print_step, T dt, T dx, T dy, T beta0, BoundaryCond bound)
+{
+    T *uc, *vc, *un, *vn, *P;
+    initialize(uc, vc, un, vn, P, bound);
+    T beta = beta0 / (2*dt*(1/(dx*dx) + 1/(dy*dy)));
+
+    T accumulate_time = 0;
+    T last_printed = 0;
+    int index = 0;
+    print_velocity(uc, vc, index++);
+    while (accumulate_time < total_time) {
+        accumulate_time += dt;
+        time_step(uc, vc, P, un, vn, dt, dx, dy, beta, bound);
+
+        std::swap(uc, un);
+        std::swap(vc, vn);
+        if (accumulate_time >= last_printed + print_step) {
+            last_printed += print_step;
+            print_velocity(uc, vc, index++);
+        }
+    }
+}
+
+int main()
+{
+    float total_time = 2.0, print_step = 0.1;
+    SimpleBoundary<float> sb { 25, 80, 25, 50, 1.0f, 0.0f };
+    float dx = 0.01, dy = 0.01, dt = 0.0001;
+    float beta0 = 1.7f;
+
+    flow(total_time, print_step, dt, dx, dy, beta0, sb);
+}
