2d506403be861c2f90d29d3833f06d924d0dba6d
boss14420 <firefoxlinux@gmail.com>
2015-05-27 12:35:39 +0700

    update

--- GPU/testblocksize.sh
+++ GPU/testblocksize.sh
@@ -26,34 +26,43 @@ RESFILE="$PROG_HOME/testblocksize.csv"
 
 export LC_NUMERIC="en_US.UTF-8"
 
-printf "X,Y,time\n" > "$RESFILE"
+#printf "X,Y,time\n" > "$RESFILE"
+rm -rf "$RESFILE"
 sed -rie "s/^#define cellPerThreadX .*/#define cellPerThreadX 2/" "$SRCFILE"
 sed -rie "s/^#define cellPerThreadY .*/#define cellPerThreadY 8/" "$SRCFILE"
 sed -rie "s/^#define STRIDE .*/#define STRIDE 1024/" "$SRCFILE"
 sed -rie "s/^#define WIDTH .*/#define WIDTH 1023/" "$SRCFILE"
 sed -rie "s/^#define HEIGHT .*/#define HEIGHT 1023/" "$SRCFILE"
 
-for X in 1 2 4 8 16 32
+printf "X/Y,"
+for X in 1 2 4 6 8 16 32
 do
-    for Y in 1 2 4 8 16 32
-    do
-		if (( $X*$Y % 32 == 0 ))
-		then
-			printf "Block size (%2s, %2s): " $X $Y
-			sed -rie "s/^#define threadPerBlockX .*/#define threadPerBlockX $X/" "$SRCFILE"
-			sed -rie "s/^#define threadPerBlockY .*/#define threadPerBlockY $Y/" "$SRCFILE"
-			make 2>&1 >/dev/null
+    printf "%2s" $X >> "$RESFILE"
+done
 
-			t1=$(/usr/bin/time -f "%e" "$PROG" 2>&1 >/dev/null)
-	                printf "%.02f, " $t1
-			sleep 1
-			t2=$(/usr/bin/time -f "%e" "$PROG" 2>&1 >/dev/null)
-	                printf "%.02f, " $t2
-			sleep 1
-			t3=$(/usr/bin/time -f "%e" "$PROG" 2>&1 >/dev/null)
-			TIME=$(echo "($t1+$t2+$t3)/3" | bc -l)
-	                printf "%.02f,  avg: %.03f\n" $t3 $TIME
-			printf "%2s,%2s,%.03f\n" $X $Y $TIME >> "$RESFILE"
-		fi
+for Y in 1 2 4 6 8 16 32
+do
+    printf ",%2s" $Y >> "$RESFILE"
+    for X in 2 4 6 8 16 32
+    do
+            if (( $X*$Y % 32 == 0 ))
+            then
+                    printf "Block size (%2s, %2s): " $X $Y
+                    sed -rie "s/^#define threadPerBlockX .*/#define threadPerBlockX $X/" "$SRCFILE"
+                    sed -rie "s/^#define threadPerBlockY .*/#define threadPerBlockY $Y/" "$SRCFILE"
+                    make 2>&1 >/dev/null
+
+                    t1=$(/usr/bin/time -f "%e" "$PROG" 2>&1 >/dev/null)
+                    printf "%.02f, " $t1
+                    sleep 1
+                    t2=$(/usr/bin/time -f "%e" "$PROG" 2>&1 >/dev/null)
+                    printf "%.02f, " $t2
+                    sleep 1
+                    t3=$(/usr/bin/time -f "%e" "$PROG" 2>&1 >/dev/null)
+                    TIME=$(echo "($t1+$t2+$t3)/3" | bc -l)
+                    printf "%.02f,  avg: %.03f\n" $t3 $TIME
+                    printf ",%.03f" $TIME >> "$RESFILE"
+            fi
     done
+    printf "\n" >> "$RESFILE"
 done
